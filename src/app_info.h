ee425dbeb9b07a9714c2a89345e6a9199eb864a1
Jose M Rico <jmrico01@gmail.com>
2019-09-05 00:16:18 -0400

    stb + km_platform progress

---
+++ src/app_info.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <km_common/km_defines.h>
+
+#define APP_315K
+
+global_var const char* APP_NAME = "315k";
+global_var const uint64 PERMANENT_MEMORY_SIZE = MEGABYTES(256);
+global_var const uint64 TRANSIENT_MEMORY_SIZE = GIGABYTES(1);
--- src/load_png.h
+++ src/load_png.h
@@ -1,19 +1,21 @@
 #pragma once
 
 #include <km_common/km_math.h>
+#include <km_platform/main_platform.h>
 
-#include "main_platform.h"
 #include "opengl.h"
 
 struct TextureGL
 {
-    Vec2Int size;
-    GLuint textureID;
+	Vec2Int size;
+	GLuint textureID;
 };
 
+void UnloadTextureGL(const TextureGL& textureGL);
+
 // TODO change ref out to pointer
-bool32 LoadPNGOpenGL(const ThreadContext* thread, const char* filePath,
-    GLint magFilter, GLint minFilter, GLint wrapS, GLint wrapT,
-    TextureGL& outTextureGL, MemoryBlock transient,
-    DEBUGPlatformReadFileFunc* DEBUGPlatformReadFile,
-    DEBUGPlatformFreeFileMemoryFunc* DEBUGPlatformFreeFileMemory);
+bool LoadPNGOpenGL(const ThreadContext* thread, const char* filePath,
+	GLint magFilter, GLint minFilter, GLint wrapS, GLint wrapT,
+	TextureGL& outTextureGL, MemoryBlock transient,
+	DEBUGPlatformReadFileFunc* DEBUGPlatformReadFile,
+	DEBUGPlatformFreeFileMemoryFunc* DEBUGPlatformFreeFileMemory);
